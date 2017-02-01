435da1f10d48389f1a3456af75fc9d61c1e99891
Mateusz  Krupa <mateuszkrupa@MacBook-Pro-Mateusz.local>
2017-02-01 22:52:43 +0100

    d3js POC

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,31 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component, PropTypes } from 'react';
+'use strict';
+
+import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
+  ScrollView,
+  TouchableWithoutFeedback,
   View,
-  ScrollView
 } from 'react-native';
+
+import Pie from './Pie';
+import Theme from '../../theme';
+import data from '../../resources/data';
 
 export default class Results extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      spendingsPerYear: data.spendingsPerYear,
+    };
+    this._onPieItemSelected = this._onPieItemSelected.bind(this);
+    this._shuffle = this._shuffle.bind(this);
+  }
+
+  _onPieItemSelected(newIndex){
+    this.setState({...this.state, activeIndex: newIndex, spendingsPerYear: this._shuffle(data.spendingsPerYear)});
+  }
+
+  _shuffle(a) {
+      for (let i = a.length; i; i--) {
+          let j = Math.floor(Math.random() * i);
+          [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
+      return a;
+  }
+
   render() {
+    const height = 200;
+    const width = 500;
+
     return (
-      <View>
-        <Text>RESULT</Text>
-        <Text>AAAAA</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container} >
+          <Text style={styles.chart_title}>Distribution of spending this month</Text>
+          <Pie
+            pieWidth={150}
+            pieHeight={150}
+            onItemSelected={this._onPieItemSelected}
+            colors={Theme.colors}
+            width={width}
+            height={height}
+            data={data.spendingsLastMonth} />
+          <Text style={styles.chart_title}>Spending per year in {data.spendingsLastMonth[this.state.activeIndex].name}</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-
-});
-
+const styles = {
+  container: {
+    backgroundColor:'whitesmoke',
+    marginTop: 21,
+  },
+  chart_title : {
+    paddingTop: 15,
+    textAlign: 'center',
+    paddingBottom: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    backgroundColor:'white',
+    color: 'grey',
+    fontWeight:'bold',
+  }
+}
